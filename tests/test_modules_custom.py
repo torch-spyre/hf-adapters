6d7817914a6b8f78395c2b6dd0010caa18fef611
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2026-08-10 06:02:17 +0000

    remove to skip DecoderLayer
    Signed-off-by: Kazuaki Ishizaki <ishizaki@jp.ibm.com>

--- tests/test_modules_custom.py
+++ tests/test_modules_custom.py
@@ -452,12 +452,6 @@ class TestModuleCustom(TestCase):
         4. Compares outputs between eager CPU, eager Spyre, and compile Spyre
         """
 
-        cls_name = module_info.module_cls.__name__
-        if cls_name.endswith("DecoderLayer"):
-            self.skipTest(
-                f"{cls_name}: skip the whole decoder layer due to time-consuming module"
-            )
-
         module_inputs = module_info.module_inputs_func(
             module_info, device=device, dtype=dtype, requires_grad=False, training=False
         )
@@ -588,12 +582,6 @@ class TestModuleCustom(TestCase):
         """
         run_compile = os.getenv("TEST_COMPILE_WITH_CPU", "1") == "1"
         run_eager = os.getenv("TEST_EAGER_WITH_CPU", "0") == "1"
-
-        cls_name = module_info.module_cls.__name__
-        if cls_name.endswith("DecoderLayer"):
-            self.skipTest(
-                f"{cls_name}: skip the whole decoder layer due to time-consuming module"
-            )
 
         module_inputs = module_info.module_inputs_func(
             module_info,
