b2092b8052c6b5028e618577274b03da27348fd4
Ariel Gera <ariel.gera1@ibm.com>
2026-08-27 15:40:16 +0300

    temporarily disable SDSC cache mechanism
    Signed-off-by: Ariel Gera <ariel.gera1@ibm.com>

--- hf_adapters/__init__.py
+++ hf_adapters/__init__.py
@@ -12,6 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+import torch  # noqa: F401  (should happen before first torch_spyre import)
+
+try:
+    from torch_spyre._inductor import (  # type: ignore[import-not-found]
+        config as spyre_config,
+    )
+
+    # There is currently an issue with SDSC cache for some models
+    spyre_config.sdsc_cache = False
+except ImportError:
+    pass
+
+
 from hf_adapters.auto_spyre_model import (
     AutoSpyreModel,
     AutoSpyreModelForCausalLM,
